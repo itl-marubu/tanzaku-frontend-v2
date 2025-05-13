9a57fca108ca85bddd137cc7e730d26e53759f92
mizphses <fuminori@mizphses.com>
2025-05-14 01:11:47 +0900

    lintfix

--- src/lib/login.ts
+++ src/lib/login.ts
@@ -4,7 +4,7 @@ import { atomWithStorage } from "jotai/utils";
 export const tokenAtom = atomWithStorage<string | null>("login", null);
 export const refreshTokenAtom = atomWithStorage<string | null>(
   "refreshToken",
-  null
+  null,
 );
 
 export type User = {
